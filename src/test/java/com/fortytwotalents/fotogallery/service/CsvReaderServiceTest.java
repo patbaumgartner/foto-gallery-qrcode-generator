d7b41e43a2cea9808df9a264c193a1f84a04e69b
Patrick Baumgartner <contact@patbaumgartner.com>
2026-03-04 21:39:58 +0100

    chore: remove unused import, test-codes.csv, and MockMaker config

--- src/test/java/com/fortytwotalents/fotogallery/service/CsvReaderServiceTest.java
+++ src/test/java/com/fortytwotalents/fotogallery/service/CsvReaderServiceTest.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
